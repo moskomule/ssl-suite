a28828bec9c64dec6594f84b143870a1fdc59ba1
moskomule <hataya@keio.jp>
2020-01-11 19:07:42 +0900

    fix

--- backends/supervised_backends.py
+++ backends/supervised_backends.py
@@ -14,13 +14,14 @@ def get_dataloaders(dataset: str,
         dset, root, norm_transform, data_aug, num_cls = DATASETS[dataset]
         dset.__getitem__ = getitem
         labeled_set = dset(root, train=True, transform=transforms.Compose(data_aug + norm_transform))
+        iteration = len(labeled_set)
         with set_seed(seed):
             indices = torch.randperm(len(labeled_set))
             labeled_set.data = [labeled_set.data[i] for i in indices][:train_size]
             labeled_set.targets = [labeled_set.targets[i] for i in indices][:train_size]
         test_set = dset(root, train=False, transform=transforms.Compose(norm_transform))
         train_loader = DataLoader(labeled_set, batch_size=batch_size,
-                                  sampler=RandomSampler(labeled_set, True),
+                                  sampler=RandomSampler(labeled_set, True, num_samples=iteration),
                                   num_workers=2, pin_memory=True)
         test_loader = DataLoader(test_set, batch_size=2 * batch_size)
     else:
